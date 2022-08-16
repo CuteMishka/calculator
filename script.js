f251461650c42a69eebe7406c4f2d31c68f050c6
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-08-16 17:17:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var operation
 var resultat
 function func() {
-  var num1 = document.getElementById("number1").value;
-  var num2 = document.getElementById("number2").value;
-}
-switch (operation) {
+  var num1 = Number(document.getElementById("number1").value);
+  var num2 = Number(document.getElementById("number2").value);
+  switch (operation) {
   case "+":
-  alert(num1 + num2); 
-  break 
+    alert(num1 + num2); 
+    break;
+  case "-":
+    alert(num1 - num2);
+    break;
+  case "/":
+    alert(num1 / num2);
+    break;
+  case "*":
+    alert(num1 * num2);
+    break;
+}
 }
